7f1b8d3fa90dcb1fb6dc24523e4ee62eedd0cfc0
Ramy Nasr <ramy.nasr@automattic.com>
2025-12-02 15:04:32 -0500

    feat: register initiatives commands in factory

--- src/commands/factory.ts
+++ src/commands/factory.ts
@@ -6,6 +6,8 @@ import { list as projectsList } from './projects/list/index.ts';
 import { show as projectsShow } from './projects/show/index.ts';
 import { showMyUpdates as projectsShowMyUpdates } from './projects/show-my-updates/index.ts';
 import { list as notificationsList } from './notifications/list/index.ts';
+import { list as initiativesList } from './initiatives/list/index.ts';
+import { show as initiativesShow } from './initiatives/show/index.ts';
 
 export type CommandHandler<TOptions = Record<string, unknown>> = (
   client: GraphQLClient,
@@ -24,6 +26,10 @@ const commandMap: Record<string, Record<string, CommandHandler>> = {
   },
   notifications: {
     list: notificationsList,
+  },
+  initiatives: {
+    list: initiativesList,
+    show: initiativesShow,
   },
 };
 
